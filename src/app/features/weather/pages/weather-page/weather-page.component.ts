c40d59fea4b962b718d6aefd66bcc48da1012916
Árpád Soós <arpad.soos@icellmobilsoft.hu>
2025-03-03 17:05:47 +0100

    Implementing Layout (WIP)

--- src/app/features/weather/pages/weather-page/weather-page.component.ts
+++ src/app/features/weather/pages/weather-page/weather-page.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainLayoutComponent } from '@core/layout/main-layout/main-layout.component';
 import { WebshopHeaderComponent } from 'src/app/features/webshop/components/webshop-header/webshop-header.component';
+import { FooterComponent } from "../../../../core/components/footer/footer.component";
 
 @Component({
   selector: 'app-weather-page',
-  imports: [CommonModule, RouterOutlet, MainLayoutComponent, WebshopHeaderComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    MainLayoutComponent, 
+    WebshopHeaderComponent, 
+    FooterComponent
+  ],
   templateUrl: './weather-page.component.html',
   styleUrl: './weather-page.component.scss'
 })
--- src/app/features/webshop/pages/webshop-page/webshop-page.component.ts
+++ src/app/features/webshop/pages/webshop-page/webshop-page.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainLayoutComponent } from '@core/layout/main-layout/main-layout.component';
 import { WebshopHeaderComponent } from '../../components/webshop-header/webshop-header.component';
+import { FooterComponent } from "../../../../core/components/footer/footer.component";
 
 @Component({
   selector: 'app-webshop-page',
-  imports: [CommonModule, RouterOutlet, MainLayoutComponent, WebshopHeaderComponent],
+  imports: [CommonModule, RouterOutlet, MainLayoutComponent, WebshopHeaderComponent, FooterComponent],
   templateUrl: './webshop-page.component.html',
   styleUrl: './webshop-page.component.scss'
 })
